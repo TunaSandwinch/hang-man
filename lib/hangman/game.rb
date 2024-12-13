2cbb6183000df1433fec0dbc4150688f46cc5e05
TunaSandwinch <winch.mascarenas@gmail.com>
2024-12-13 12:56:26 +0800

    feat: game class can now set the instance variables of the player class

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -1,10 +1,15 @@
+require_relative 'player.rb'
 class Game
   @@word_pool = File.readlines('words.txt', chomp: true)
   attr_accessor :secret_word
-  def initialize
+  def initialize(player)
     @secret_word = random_word
+    @player = player
+    initial_guessed_words(player)
+    player.lives = 6
   end
 
+  private
   def random_word
     secret_word = @@word_pool[rand(0..999)]
     if secret_word.length >= 5 && secret_word.length <= 12
@@ -13,8 +18,15 @@ class Game
       random_word
     end
   end
+
+  def initial_guessed_words(player)
+    secret_word.length.times do
+      player.guessed_words << '-'
+    end
+  end
 end
 
-test = Game.new
-p test.secret_word
+player = Player.new
+game = Game.new(player)
+p player.guessed_words
 
