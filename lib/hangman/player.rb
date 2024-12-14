77717ac430acf3c9d84324091fb42f66bd4ea5e2
TunaSandwinch <winch.mascarenas@gmail.com>
2024-12-14 20:58:23 +0800

    feat: added serialization methods to the class

--- lib/hangman/player.rb
+++ lib/hangman/player.rb
@@ -1,8 +1,26 @@
+require 'yaml'
 class Player
   attr_accessor :lives, :guessed_letters
   def initialize
     @lives = nil
     @guessed_letters = []
+  end
+
+  def make_guess
+    puts ' make your guess:'
+    gets.chomp.downcase
+  end
+
+  def to_yaml
+    YAML.dump({
+      :lives => lives,
+      :guessed_letters => guessed_letters,
+    })
+  end
+
+  def self.from_yaml(file)
+    contents = YAML.load(file)
+    self.new(contents[:lives], contents[:guessed_letters])
   end
 end
 puts 'player.rb has loaded :>'
